95a6b012ef0b579b15d2db0ba6f7fe77de9dd640
Eric Richardson <eric@ericonrails.com>
2016-06-27 11:52:08 -0700

    Answer JavaScript bonus question

---
+++ bonus.js
@@ -0,0 +1,13 @@
+var filter = function (arr, filt){
+  filtArr = [];
+
+  for(var i = 0; i < arr.length; i++){
+    if(filt(arr[i])){
+      filtArr.push(arr[i])
+    };
+  };
+  return filtArr;
+}
+
+var isEven = function (x) { return x % 2 == 0; };
+console.log(filter([1, 2, 3, 4], isEven)); // => [2, 4]
